f72b090896013ba963236e9659d6e84df5c8806e
Subhashini S <subhashinisiva16@gmail.com>
2025-10-04 17:06:38 +0530

    Create Print_duplicates.c
    Print Duplicates
    
    Problem Statement
    Write a program to print the duplicate characters in the given string
    
    Input Format
    Given a string
    
    Output Format
    print the duplicate characters separated by space ':'
    
    Constraint :
    1 <= string length <= 100
    
    Note: If more than one occurred then print the first occurrence
    
    the string might have upper case letter that should be considered as lowercase
    
    Sample Input 1:
    This iS a test Sentence
    
    Sample Output 1:
    t i s e n

---
+++ Print_duplicates.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main(){
+    char str[101];
+    int freq[26]={0};
+    int printed[26]={0};
+    
+    scanf("%[^\n]s",str);
+    
+    for(int i=0;str[i]!='\0';i++){
+        char ch=str[i];
+        
+        if(ch>='A' && ch<='Z'){
+            ch=ch+('a'-'A');
+        }
+        if(ch>='a' && ch<='z'){
+            freq[ch-'a']++;
+        }
+    }
+    
+    for(int i=0;str[i]!='\0';i++){
+        char ch=str[i];
+        
+        if(ch>='A' && ch<='Z'){
+            ch=ch+('a'-'A');
+        }
+        
+        if(ch>='a' && ch<='z'){
+            if(freq[ch-'a']>1 && !printed[ch-'a']){
+                printf("%c ",ch);
+                printed[ch-'a']=1;
+            }
+        }
+    }
+    return 0;
+}
